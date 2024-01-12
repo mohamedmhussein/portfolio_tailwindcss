e3709538c9a1448d82f05b8d544937c7c7d43546
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-01-12 18:03:41 -0500

    Adds Nav names when hovering over icons

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -39,9 +39,17 @@ const Nav = () => {
 
         {data.map(link => {
           return <Link 
-            className= {`${link.path === pathname && 'text-accent'}`}
+            className= {`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300  `}
             href={link.path} 
             key={link.path}>
+              
+              <div className='absolute pr-14 hidden xl:group-hover:flex right-0'>
+                <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>{link.name}</div>
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              
               <div>{link.icon}</div>
           </Link>
         })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
         secondary: '#393A47',
         accent: '#F13024',
       },
+      backgroundImage: {
+        explosion: 'url("/bg-explosion.png")',
+        circles: 'url("/bg-circles.png")',
+        circleStar: 'url("/circle-star.svg")',
+        site: 'url("/site-bg.svg")',
+      }
     },
   },
   plugins: [],
