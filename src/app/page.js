34626d6efa3f411b9f3b103ce9a2f90e93316a0b
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-01-15 18:52:35 -0500

    Sets up page transitions

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
+'use client'
+import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
+import Transition from '../components/Transition'
 
 export default function Home() {
   return (
-    <main className="">
-      
-
-    </main>
+    <div>Home</div>
   )
 }
