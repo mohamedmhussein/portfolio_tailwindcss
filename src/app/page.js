eb87674e03efaa72b2f945712999116150a11e52
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-01-16 11:44:00 -0500

    Sets the JSX structure of the home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,20 @@ import Transition from '../components/Transition'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-primary/60' >
+      {/* text */}
+      <div>
+        <div>
+          <h1 className=''>
+            Transforming ideas <br /> Into 
+            Transforming ideas <br /> Into <span className='text-accent'>Digital Reality</span>
+          </h1>
+          <p>
+            sdfdsfash aggjfhewrbcv dgsdfht dgsdf  fgfdgsdg ea  rfsdgsdfgs d
+          </p>
+        </div>
+      </div>
+      {/* image */}
+    </div>
   )
 }
