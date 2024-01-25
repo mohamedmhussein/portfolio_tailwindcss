f1e0db25764f7b071c2a4ebf0cc7b2a6fb2283e7
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-01-25 16:35:32 -0500

    Finalizing the avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
-        
+        <Image src='/Avatar.png' width={737} height={678} alt='' className='translate-z-0 w-full h-full scale-[0.7]'/>
     </div>
   )
 }
